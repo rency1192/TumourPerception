000121923258d3ed504ce63f7b5f318e0dc68660
Rency Kapadia <20it051@charusat.edu.in>
2023-03-28 00:36:25 +0530

    Flask App

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the pre-trained model
+model = tf.keras.models.load_model('templates/my_model.h5')
+
+# Define the labels for the classes
+class_names = ['Benign', 'Malignant']
+
+
+# index.html page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# predict and display it on result.html page
+@app.route('/upload', methods=['POST'])
+def upload():
+    image = request.files['file']
+    image = Image.open(image)
+    image = image.resize((180, 180))
+    img_array = tf.keras.utils.img_to_array(image)
+    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+    predictions = model.predict(img_array)
+    score = tf.nn.softmax(predictions[0])
+    result = class_names[np.argmax(score)]
+    percentage = 100 * np.max(score)
+    return render_template('results.html', result=result, percentage=percentage)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
